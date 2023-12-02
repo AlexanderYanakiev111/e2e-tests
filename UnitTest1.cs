b8c6d54afa69d3617bf17fbf822739ee3e0cb2ca
AlexanderYanakiev111 <agyanakiev.workspace@gmail.com>
2023-12-02 19:25:29 +0200

    e2e test initial

---
+++ UnitTest1.cs
@@ -0,0 +1,37 @@
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework.Constraints;
+
+namespace PlaywrightTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        using var playwright = await Playwright.CreateAsync();
+
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions{
+            Headless = false
+        });
+
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("http://www.eaapp.somee.com");
+        await page.ClickAsync("text = Login"); 
+        await page.ScreenshotAsync(new PageScreenshotOptions
+        {
+            Path = "EAApp.jpg"
+        });
+
+        await page.FillAsync("#UserName", "admin");
+        await page.FillAsync("#Password", "password");
+        await page.ClickAsync("text = Log in");
+        var isExist = await page.Locator("text=Employee Details").IsVisibleAsync();
+        Assert.IsTrue(isExist);
+    }
+}
